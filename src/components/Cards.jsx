b946fd40ee0130dab8a1f4d71c0e500aed671ed6
Oroxhimaru <hm51562@gmail.com>
2023-12-29 12:32:00 +0500

    project finish

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
 import { IoClose } from "react-icons/io5"
+import { motion } from "framer-motion"
 
-const Cards = ({data}) => {
+const Cards = ({data, reference}) => {
   return (
-    <div className='relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
+    <motion.div drag dragConstraints={reference} whileDrag={{scale:1.2}} dragElastic={0.2} dragTransition={{ bounceStiffness: 100, bounceDamping: 10}} className='relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden flex-shrink-0'>
         <FaRegFileAlt />
            <p className='text-sm leading-tight mt-5 font-semibold'>{data.desc}</p>
            <div className='footer absolute bottom-0 w-full left-0'>
@@ -15,11 +16,15 @@ const Cards = ({data}) => {
                 {data.close ? <IoClose /> : <LuDownload size=".7em" color='#fff' />}
                 </span>
              </div>
-             <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
-                <h3 className='text-sm font-semibold'>Download Now</h3>
+             {
+               data.tag.isOpen && (
+             <div className={`tag w-full py-4 ${data.tag.tagColor === 'blue' ? "bg-blue-600" : 'bg-green-600'} flex items-center justify-center`}>
+                <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
              </div>
+               ) }
+
            </div>
-    </div>    
+    </motion.div>    
     )
 }
 
--- src/components/Frontpage.jsx
+++ src/components/Frontpage.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Cards from "./Cards";
 
 const Frontpage = () => {
+
+ const ref = useRef(null);
+
   const data = [
     {
       desc: "jbasjdbas asjbfkjsfb askfnsajdas sjfabfbdj djfd sajdfbas",
@@ -11,12 +14,29 @@ const Frontpage = () => {
         isOpen: true, tagTitle: "Download Now", tagColor: "green"
       },
     },
+    {
+      desc: "jbasjdbas asjbfkjsfb askfnsajdas sjfabfbdj djfd sajdfbas",
+      filesize: ".9mb",
+      close: true,
+      tag: {
+        isOpen: false, tagTitle: "Download Now", tagColor: "green"
+      },
+    },
+    {
+      desc: "jbasjdbas asjbfkjsfb askfnsajdas sjfabfbdj djfd sajdfbas",
+      filesize: ".9mb",
+      close: true,
+      tag: {
+        isOpen: true, tagTitle: "Download Now", tagColor: "blue"
+      },
+    },
   ];
+  console.log(data.map(item => item.tag.tagColor));
   return (
     <>
-      <div className="fixed top-0 left-0 z-[3] w-full h-full">
+      <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5">
       {data.map((item, index) => (
-          <Cards key={index} data={item} />
+          <Cards key={index} data={item} reference={ref} />
         ))}
       </div>
     </>
